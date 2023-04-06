a0a74c99cd341ae812afcd1e0838279be97ec88c
priya <vijayapriya491@gmail.com>
2023-04-06 11:58:13 +0530

    add files

--- php/request.php
+++ php/request.php
@@ -1,12 +1,16 @@
 <?php
 
-require_once __DIR__ .'/headerClass.php';
-$request = new CrudClass();
+require_once __DIR__ .'/csvClass.php';
+$request = new csvClass();
 $request->headerrequest();
 $request_method = strtolower($_SERVER['REQUEST_METHOD']);
-$request_path = strtolower($_SERVER['PATH_INFO']);
-//echo $request_path;
-//echo "<pre>";print_r($_SERVER['PATH_INFO']);
+
+if(isset($_SERVER['PATH_INFO'])) {
+	$request_path = strtolower($_SERVER['PATH_INFO']);
+}else{
+	$request_path = '/list';
+}
+
 if($request_path == '/list') {
 	$message 	= $request->listcCsv();
 	echo $message;
@@ -19,11 +23,15 @@ if($request_path == '/add') {
 }
 
 if($request_path == '/delete') {
-	//echo "deleted";
 	 $object = file_get_contents("php://input");
 	 $message = $request->deleteCsv($object);
 	 $message 	= $request->listcCsv();
 	 echo $message;
-}	
+}
+if($request_path == '/checkitem'){
+	$object = file_get_contents("php://input");
+	$message = $request->checkCsv($object);
+	echo $message;
+}
 
 ?>
